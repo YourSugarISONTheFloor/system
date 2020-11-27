797c1617a18255802426211df2a73891d2a058a6
饭团 <1317524710@qq.com>
2020-11-27 18:02:04 +0800

    thymeleaf国际化

--- src/main/java/cn/fantuan/system/controller/IndexController.java
+++ src/main/java/cn/fantuan/system/controller/IndexController.java
@@ -5,11 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    //后台首页
     @RequestMapping("/welcome")
     public String welcome() {
         return "/subsidiary/welcome-1";
     }
-
+    //登录页面
     @RequestMapping("/login")
     public String logo() {
         return "/subsidiary/login";
